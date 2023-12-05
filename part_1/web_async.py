12b67e137c7b4e7fa6fc493e4e12ff6d1e04754a
ahugo <hugo.andriamampianina@ynov.com>
2023-12-05 11:42:04 +0100

    update

--- part_1/web_async.py
+++ part_1/web_async.py
@@ -31,8 +31,9 @@ async def main():
     url = argv[1]
     file_path = "/tmp/web_page"
     
-    
-    tasks = [ get_content(url), write_content(await get_content(url), file_path) ]
+    content_task = get_content(url)
+    write_task = write_content(await content_task, file_path)
+    tasks = [ content_task, write_task ]
     asyncio.gather(*tasks)
     
     
