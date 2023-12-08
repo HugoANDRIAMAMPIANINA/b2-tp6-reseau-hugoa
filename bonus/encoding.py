dfb6533b3746305fd373a94ca724c5f178dec9b1
ahugo <hugo.andriamampianina@ynov.com>
2023-12-08 11:28:01 +0100

    update

--- bonus/encoding.py
+++ bonus/encoding.py
@@ -5,6 +5,7 @@ def encode_message(message:str) -> bytes:
     message_len_bit_length = message_len.bit_length()
     message_len_byte_length = 2 if ceil(message_len_bit_length/8.0) < 3 else ceil(message_len_bit_length/8.0)
     message_len_length = 1 if message_len_byte_length < 3 else 2
+    print(f"{message_len_length} {message_len}")
     
     header = message_len_length.to_bytes(1, byteorder='big')
     sequence = header + message_len.to_bytes(message_len_byte_length, byteorder='big') + message.encode()
